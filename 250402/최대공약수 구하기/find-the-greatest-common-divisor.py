1aa79854ff15ff4e16893f2372ef7488a98ec831
pinggu95 <chaji0921@naver.com>
2025-04-02 18:04:26 +0900

    Title: 최대공약수 구하기, Time: 40ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250402/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+max_num = max(n,m)
+gcd =1
+for i in range (1,max_num):
+    if n % i ==0 and m%i ==0:
+        gcd = i
+
+print(gcd)        
+# Please write your code here.
