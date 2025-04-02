0e5f5b29f5bf5f635afbaa03cb7b8c53333965e4
pinggu95 <chaji0921@naver.com>
2025-04-02 18:08:25 +0900

    Title: 최대공약수 구하기, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250402/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 250402/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,14 +1,13 @@
 n, m = map(int, input().split())
+
 max_num = max(n,m)
 gcd =1
+for i in range (1,max_num):
+    if n % i ==0 and m%i ==0:
+        gcd = i
 
-def gcd_num():
-    for i in range (1,max_num):
-        if n % i ==0 and m%i ==0:
-            gcd = i
-            print(gcd)
+print(gcd)  
 
-gcd_num()
 
       
 # Please write your code here.
