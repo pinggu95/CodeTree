9d9d4986bb2985b6fac0641972863e5083406fac
pinggu95 <chaji0921@naver.com>
2025-04-03 16:00:20 +0900

    Title: 최소공배수 구하기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250403/최소공배수 구하기/find-the-least-common-multiple.py
+++ 250403/최소공배수 구하기/find-the-least-common-multiple.py
@@ -2,13 +2,12 @@ n, m = map(int, input().split())
 
 def gcd(n, m):
     min_num = min(n, m)
-    gcd = 1
+    gcda = 1
     for i in range(1, min_num + 1):
         if n % i == 0 and m % i == 0:
-            gcd = i
-    return gcd
+            gcda = i
+    return gcda
 
 def lcm(n, m):
     return abs(n * m) // gcd(n, m)
-
-lcm()
+lcm(n, m)
